d4092d1a86681ff1dcd0edfc75d4b7f3790d07b9
Chintan Sartape <ChintanS2@citiustech.com>
2021-04-16 12:39:49 +0530

    login logic

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -82,4 +82,34 @@ router.delete('/:id', (req, res) => {
 
 })
 
+router.post('/login', (req, res) => {
+
+    // Employee.findByCredentials(req.body.email, req.body.password)
+    email = req.body.email
+    password = req.body.password
+
+    Employee.findOne({ email: email })
+    .then((emp) => {
+        if (!emp) {
+            // throw new Error('Unable to Login')
+            res.status(404).send("Employee email Not Found")
+        } else {
+
+            if (emp.password !== password) {
+                // throw new Error('Unable to Login')
+                res.status(404).send("Password not match")
+                
+            } else {
+
+                res.status(200).send(emp)
+            }
+        }
+        
+
+    })
+    .catch(error => res.status(500).send(error))
+
+
+})
+
 module.exports = router;
